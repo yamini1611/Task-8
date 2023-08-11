1897bafa7b00b4bc2c937f6d22e23b888057b731
yamini1611 <yaminipriya1611@gmail.com>
2023-08-11 17:05:45 +0530

    Add project files.

---
+++ Task 8/Delegate.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CouponExample
+{
+    delegate double ApplyCoupon(double totalCost, bool hasValidCoupon);
+
+    class Program
+    {
+        static bool IsValidCoupon(string couponCode)
+        {
+         
+            return couponCode == "VALIDCOUPON";
+        }
+
+        static double ValidAmount(double totalCost, bool hasValidCoupon)
+        {
+            const double couponThreshold = 2000;
+            const double couponAmount = 100; 
+
+            if (hasValidCoupon && totalCost > couponThreshold)
+            {
+                return totalCost - couponAmount;
+            }
+
+            else
+            {
+
+                return totalCost;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ApplyCoupon couponDelegate;
+            Console.WriteLine("Enter no of Customers:");
+            int customers = int.Parse(Console.ReadLine());
+            for(int  i = 0; i < customers; i++)
+            {
+                Console.WriteLine("Enter Customer Name:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter total purchase cost:");
+                double totalCost = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter coupon code:");
+                string couponCode = Console.ReadLine();
+
+                bool hasValidCoupon = IsValidCoupon(couponCode);
+
+                if (totalCost > 2000 && hasValidCoupon)
+                {
+                    Console.WriteLine("Coupon Applied");
+                    couponDelegate = ValidAmount;
+                }
+                else if (totalCost < 2000)
+                {
+                    Console.WriteLine("Amount is below the range for applying coupon");
+                    couponDelegate = ValidAmount;
+                }
+                else if (!hasValidCoupon)
+                {
+                    Console.WriteLine("Invalid Code");
+                    couponDelegate = ValidAmount;
+                }
+                else
+                {
+                      couponDelegate = ValidAmount;
+                }
+                double finalCost = couponDelegate(totalCost, hasValidCoupon);
+
+                Console.WriteLine($"Final Cost after applying coupon: {finalCost}");
+            }
+      
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace TodoListExample
+{
+    class Program
+    {
+        class Task
+        {
+            public string Description { get; set; }
+            public bool IsCompleted { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<Task>> currentbatchTasks = new Dictionary<string, List<Task>>
+            {
+                { "2020", new List<Task>
+                    {
+                        new Task { Description = "Complete Assignment 1", IsCompleted = false },
+                        new Task { Description = "Study for Quiz", IsCompleted = false }
+                    }
+                },
+                { "2021", new List<Task>
+                    {
+                        new Task { Description = "Prepare Presentation", IsCompleted = false }
+                    }
+                },
+                { "2022", new List<Task>
+                    {
+                        new Task { Description = "Attend Workshop", IsCompleted = false },
+                        new Task { Description = "Review Notes", IsCompleted = false }
+                    }
+                }
+            };
+
+            Action<string> showTasks = currentbatch2 =>
+            {
+                if (currentbatchTasks.ContainsKey(currentbatch2))
+                {
+                    Console.WriteLine($"Tasks for currentbatch {currentbatch2}:");
+                    foreach (var task in currentbatchTasks[currentbatch2])
+                    {
+                        string status = task.IsCompleted ? "Completed" : "Pending";
+                        Console.WriteLine($"- {task.Description} (Status: {status})");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"No tasks found for currentbatch {currentbatch2}.");
+                }
+            };
+
+            Action<string> updateTaskStatus = taskDescription =>
+            {
+                foreach (var currentbatch1 in currentbatchTasks.Keys)
+                {
+                    foreach (var task in currentbatchTasks[currentbatch1])
+                    {
+                        if (task.Description == taskDescription)
+                        {
+                            task.IsCompleted = true;
+                            Console.WriteLine($"Task '{task.Description}' marked as completed.");
+                            return;
+                        }
+                    }
+                }
+                Console.WriteLine($"Task '{taskDescription}' not found.");
+            };
+
+            Console.WriteLine("Enter your currentbatch (2020, 2021, or 2022) or 'q' to quit:");
+            string currentbatch = Console.ReadLine();
+
+            if (currentbatch == "2020" || currentbatch == "2021" || currentbatch == "2022")
+            {
+                showTasks(currentbatch);
+
+                Console.WriteLine("Enter a task description to mark as completed (or 'q' to quit):");
+                string taskDescription = Console.ReadLine();
+
+                if (taskDescription.ToLower() != "q")
+                {
+                    updateTaskStatus(taskDescription);
+                }
+            }
+            else if (currentbatch.ToLower() != "q")
+            {
+                Console.WriteLine("Invalid currentbatch.");
+            }
+
+         
+            Console.ReadLine();
+        }
+
+    }
+}
+
+
+namespace MultiplicationTable
+{
+    class Table
+    {
+        static int MultiTable(int x , int y)
+        {
+            return x * y;
+        }
+        public void Multiply()
+        {
+            int result = 0;
+            Console.WriteLine("Enter the number ");
+            int x =int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------");
+            Func<int, int, int> Table = MultiTable;
+            for(int i = 1; i <= 10;i++)
+            {
+                result = Table(x,i);
+                Console.WriteLine($"{x} * {i} = {result}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Table func = new Table();
+            func.Multiply();
+            Console.WriteLine("---------------------------");
+            Console.ReadLine();
+        }
+    }
+
+    
+}
---
+++ Task 8/File.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Task_8
+{
+    internal class Program
+    {
+        public int id;
+        public string name;
+        public string Degree;
+        public double salary;
+        public double bonus;
+        public string Details;
+
+        public void Trainee()
+        {
+            Console.WriteLine("Enter Employee id:");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Trainee Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Trainee Degree: ");
+            Degree = Console.ReadLine();
+            Console.WriteLine("Enter Trainee Salary: ");
+            salary = Convert.ToDouble(Console.ReadLine());
+
+           
+            bonus = salary * 0.1; 
+
+            Details += ($"\nTrainee {name}'s id is {id}, trainee has Degree {Degree}, salary is {salary}, and bonus is {bonus}");
+            Console.WriteLine(Details);
+        }
+        public void display()
+        {
+            FileInfo file = new FileInfo(@"E:/C# Read Write.txt");
+            FileStream fileStream = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            StreamWriter writer = new StreamWriter(fileStream);
+            try
+            {
+                writer.WriteLine(Details);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Close();
+                fileStream.Close();
+            }
+        }
+        public void GenerateCSV(List<string> lines)
+        {
+            FileInfo file = new FileInfo(@"E:/C# Report.csv");
+            FileStream fileStream = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            StreamWriter writer = new StreamWriter(fileStream);
+            foreach (string line in lines)
+            {
+                writer.WriteLine(line);
+            }
+            writer.Close();
+            fileStream.Close();
+        }
+
+        public void DisplayCSV(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        public void Append()
+        {
+            FileInfo file = new FileInfo(@"E:/C# Read Write.txt");
+            FileStream fileStream = file.Open(FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            StreamWriter writer1 = new StreamWriter(fileStream);
+            try
+            {
+  
+                writer1.WriteLine($"\nTrainee {name}'s id is {id} and trainee has Degree {Degree}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                writer1.Close();
+                fileStream.Close();
+            }
+        }
+
+        public void Copy()
+        {
+            string SourceFile = @"E:/C# Read Write.txt";
+            string destinationFile = @"E:/C# Copytext.txt";
+
+            try
+            {
+                File.Copy(SourceFile, destinationFile);
+                Console.WriteLine("copied Successfully");
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
+        }
+        public void Move()
+        {
+            string SourceFile = @"E:/C# Copytext.txt";
+            string destinationFile = @"E:/C# Move.txt";
+            try
+            {
+                File.Move(SourceFile, destinationFile);
+                Console.WriteLine("Moved Successfully");
+            }
+            catch (Exception ei)
+            {
+                Console.WriteLine(ei.Message);
+            }
+
+        }
+        public void Delete()
+        {
+            string file = @"E:/C# Copytext.txt";
+            if (File.Exists(file))
+            {
+                Console.WriteLine("file exists , so deleting the file");
+                File.Delete(file);
+            }
+            else
+            {
+                Console.WriteLine("file does not exist so creating a file");
+                FileInfo file1 = new FileInfo(@"E:/C# Copytext.txt");
+                FileStream fileStream = file1.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                StreamWriter writer = new StreamWriter(fileStream);
+
+                try
+                {
+                    writer.WriteLine("file deleted and created successfully");
+                    Console.WriteLine(file1);
+                }
+                catch (Exception y)
+                {
+                    Console.WriteLine(y.Message);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Trainee();
+
+            Console.WriteLine("Enter number of employees:");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            List<string> employeeLines = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                program.Trainee();
+                employeeLines.Add($"{program.id},{program.name},{program.Degree},{program.salary},{program.bonus}");
+            }
+
+            string csvFilePath = @"E:/C# Report.csv";
+
+            program.GenerateCSV(employeeLines);
+            program.display();
+            Console.WriteLine("-----------------------");
+            program.Append();
+            Console.WriteLine("-----------------------");
+            program.Copy();
+            Console.WriteLine("-----------------------");
+            program.Move();
+            Console.WriteLine("-----------------------");
+            program.Delete();
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Employee details stored in CSV file:");
+            program.DisplayCSV(csvFilePath);
+            Console.WriteLine("-----------------------");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine($"Application Name: {assembly.GetName().Name}");
+            Console.WriteLine($"Version: {assembly.GetName().Version}");
+            Console.WriteLine($"Location: {assembly.Location}");
+
+            Console.ReadLine();
+        }
+    }
+}
